1c78c6caea60829fbf9330696266591394fa6e80
Aditya Srivastava <adityasrivastaava@gmail.com>
2019-10-04 22:37:18 +0530

    modification-process

---
+++ examp/in.js
@@ -0,0 +1,48 @@
+$(document).ready(function() {
+  $(".multiple-items").slick({
+    infinite: true,
+    slidesToShow: 5,
+    slidesToScroll: 2
+  });
+
+  $(".prev").on("click", function() {
+    $(".slick-prev")[0].click();
+  });
+
+  $(".nxt").on("click", function() {
+    $(".slick-next")[0].click();
+  });
+
+  //$('.about-title').text('About CodeBreak');
+
+  screenSlick();
+
+  $(window).resize(function() {
+    screenSlick();
+  });
+
+  function screenSlick() {
+    if ($(window).width() <= 720) {
+      $(".multiple-items").slick("unslick");
+      $(".multiple-items").slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1
+      });
+    } else if ($(window).width() <= 950 && $(window).width() > 720) {
+      $(".multiple-items").slick("unslick");
+      $(".multiple-items").slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 2
+      });
+    } else if ($(window).width() >= 951) {
+      $(".multiple-items").slick("unslick");
+      $(".multiple-items").slick({
+        infinite: true,
+        slidesToShow: 5,
+        slidesToScroll: 2
+      });
+    }
+  }
+});
