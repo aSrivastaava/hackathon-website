df4f5dd980f24ec8e77fe99d57cd5329e26eb3ac
ramsuthar305 <ramsuthar305@gmail.com>
2019-10-15 13:14:23 +0000

    /

--- dist/js.00a46daa.js
+++ dist/js.00a46daa.js
@@ -188,11 +188,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-<<<<<<< HEAD
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "38645" + '/');
-=======
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55499" + '/');
->>>>>>> 9cdfe10f50c34ba35fbc5bd165a62cadccbb2f36
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "40797" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
